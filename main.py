aa02e0f538dc0d9292393355740cbcf384ea25a1
Thijs <thijsgeertman@hotmail.com>
2021-04-15 14:58:25 +0200

    1.0.1

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from datetime import date
-from functools import wraps
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from turtle import Turtle, listen
-import keyboard
-
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
